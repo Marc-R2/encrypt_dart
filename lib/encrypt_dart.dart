c1977dbbccf2ee4c01cd84b46bd553ff0b217339
Marc Renken <marc.renken@bitbot.eu>
2023-01-01 23:44:45 +0100

    Init

---
+++ lib/encrypt_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library encrypt_dart;
+
+export 'src/encrypt_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
