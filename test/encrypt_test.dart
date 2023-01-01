e5cbdb0d622d437555bbfb811c0c51802b50a79b
Marc Renken <marc.renken@bitbot.eu>
2023-01-02 00:30:58 +0100

    Change Analysis

--- test/encrypt_test.dart
+++ test/encrypt_test.dart
@@ -3,11 +3,11 @@ import 'package:test/test.dart';
 import 'RSA/rsa_decryptor_test.dart' as rsa_decryptor;
 import 'RSA/rsa_encryption_handler_test.dart' as rsa_encryption_handler;
 import 'RSA/rsa_encryptor_test.dart' as rsa_encryptor;
+import 'hash_test.dart' as hash;
 
 Future<void> main() async {
-  group('', () {
-    rsa_encryptor.main();
-    rsa_decryptor.main();
-    rsa_encryption_handler.main();
-  });
+  rsa_encryptor.main();
+  rsa_decryptor.main();
+  rsa_encryption_handler.main();
+  hash.main();
 }
